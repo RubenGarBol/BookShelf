ebb614919782cc79f2a4300f875c959a87f7ff20
Rubén García Bolaños <ruben.g@opendeusto.es>
2020-06-10 10:45:45 +0200

    Update BookMainTest.java

--- src/test/java/main/BookMainTest.java
+++ src/test/java/main/BookMainTest.java
@@ -1,6 +1,5 @@
 package main;
 
-import java.io.IOException;
 import org.junit.Test;
 
 
@@ -10,27 +9,6 @@ public class BookMainTest {
 	  public void testMain() {
 	      String[] args = null;
 	      BookMain.main(args);
-	  }
-	
-	  @Test
-	  public void testBookMain() {
-	      try {
-			BookMain bookMain = new BookMain();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	  }
-
-	  @Test
-	  public void testOpenMain() {
-
-		  try {
-			BookMain.openMain();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}    
 	  }
 	
 
